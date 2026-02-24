dcc114cf9ceeb3095a7ab00e4ad85304c2b63d06
Satish Sheoran <sheoransatish89@gmail.com>
2026-02-24 12:03:36 +0530

    Dragging Done,Local storage saving done and items dropping change col value also in local storage

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
+// 1.new added task delete nhi ho rha h 
+// 2. initial me items load kro local storage se
+// 3. agar title,desc bda h to columns ka width grwo ho rha h and it creates a scroll bar 
+
 // getting primary color
 const root = document.documentElement;
 const primaryColor = getComputedStyle(root)
     .getPropertyValue('--primary-color').trim();
 
 // getting all the elems
-const tasks = document.querySelectorAll('.task');
+// const tasks = document.querySelectorAll('.task');
+const ToDoElem = document.querySelector('#To-Do');
+const ProgressElem = document.querySelector('#Progress');
+const CompletedElem = document.querySelector('#Completed');
 const taskColumns = document.querySelectorAll('.task-column');
 const deleteBtn = document.querySelectorAll('.delete');
+const addTaskBtn = document.querySelector('#addTask');
+
+let allTasks = JSON.parse(localStorage.getItem('Tasks')) || [];
+
 
 // initialising the task which is being dragged
 let draggingItem;
 
 // adding drag events to tasks
-tasks.forEach(task => {
-    task.addEventListener('dragstart', (e) => {
-        draggingItem = e.target;
-    });
-})
+function addDragStartEventOnTasks() {
+    const allTheTasks = document.querySelectorAll('.task');
+    allTheTasks.forEach(task => {
+        task.addEventListener('dragstart', (e) => {
+            draggingItem = e.target;
+        });
+    })
+}
+
+addDragStartEventOnTasks();
+
+//save to local storage fn
+function saveToLocal() {
+    localStorage.setItem('Tasks', JSON.stringify(allTasks))
+}
 
 // adding dragenter and dragleave event on all task columns
-taskColumns.forEach(col => {
+function addEvntToCols(col) {
     col.addEventListener('dragenter', (e) => {
-        event.preventDefault();
+        e.preventDefault();
         col.style.border = `2px dotted ${primaryColor}`
         col.style.transform = 'scale(1.05)'
     });
 
     col.addEventListener('dragleave', (e) => {
-        event.preventDefault();
+        e.preventDefault();
         col.style.border = `2px dotted transparent`
         col.style.transform = 'scale(1)'
     });
@@ -38,11 +59,18 @@ taskColumns.forEach(col => {
     });
     col.addEventListener('drop', (e) => {
         e.preventDefault();
-        e.target.appendChild(draggingItem);
+        col.appendChild(draggingItem);
         col.style.border = `2px dotted transparent`
-        col.style.transform = 'scale(1)'
+        col.style.transform = 'scale(1)';
+        let currTask = allTasks.find(task => task.title === draggingItem.children[0].innerText && task.desc === draggingItem.children[1].innerText); //getting the task which is being dragged
+        currTask.col = col.getAttribute('id'); //setting up the column in which it is dropped
+        saveToLocal(); //saving it in local storage
+
     })
-});
+}
+addEvntToCols(ToDoElem)
+addEvntToCols(ProgressElem)
+addEvntToCols(CompletedElem)
 
 //delete button working
 deleteBtn.forEach(btn => {
@@ -51,3 +79,54 @@ deleteBtn.forEach(btn => {
 
     })
 })
+
+
+
+/* new task add related */
+const addNewTaskBtn = document.querySelector('#addNewTask');
+const newtaskContainer = document.querySelector('.newtaskContainer');
+const modalBg = document.querySelector('.modalBg');
+
+
+addTaskBtn.addEventListener('click', (e) => {
+    newtaskContainer.classList.toggle('active')
+})
+
+modalBg.addEventListener('click', () => {
+    newtaskContainer.classList.remove('active')
+
+})
+
+addNewTaskBtn.addEventListener('click', (e) => {
+    const title = document.querySelector('.newTask-title');
+    const desc = document.querySelector('.newTask-desc');
+
+    // if title is empty
+    if (title.value.trim() === '') {
+        document.querySelector('.error-title').style.minHeight = "14px";
+        document.querySelector('.error-title').textContent = "Title is required";
+        return;
+    }
+    // next time if error have still styles , remove them if user enters title before clicking the add Task btn
+    document.querySelector('.error-title').style.minHeight = "0px";
+    document.querySelector('.error-title').textContent = '';
+
+    const div = document.createElement('div');
+    div.classList.add('task');
+    div.setAttribute('draggable', 'true');
+
+
+    div.innerHTML = `<h2>${title.value}</h2>
+                    <p>${desc.value}</p>
+                    <button class="delete">Delete</button>`;
+
+    ToDoElem.appendChild(div);
+    allTasks.push({ title: title.value, desc: desc.value, col: `${ToDoElem.getAttribute('id')}` })
+    saveToLocal();
+    title.value = '';
+    desc.value = '';
+    addDragStartEventOnTasks();
+    newtaskContainer.classList.remove('active');
+
+})
+/**New Task add Related */
